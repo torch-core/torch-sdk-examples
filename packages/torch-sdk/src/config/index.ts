cabeb2b6074e8117a82fa08df0abbef8ad36edd3
alan890104 <alan890104@gmail.com>
2024-12-07 23:47:49 +0800

    chore: Remove deprecated files and clean up project structure
    - Deleted unused configuration files including .babelrc, .npmignore, .npmrc, and .prettierrc from the torch-sdk package to streamline the project.
    - Removed obsolete TypeScript configuration files (tsconfig.json and tsconfig.test.json) and related test files to enhance clarity and maintainability.
    - Cleaned up the .gitignore and .yarnrc.yml files by removing unnecessary entries.
    - Eliminated various source files and tests that are no longer relevant to the current implementation.
    
    These changes help in maintaining a cleaner codebase and improving project organization.

--- packages/torch-sdk/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-export default abstract class Config {
-  static MAINNET_FACTORY_ADDRESS =
-    'EQAQN0BlMrqlgO0d8Ofi-3qq3E5oyrkBuXHFSZDT8hCTS659'
-  static MAINNET_TONHUB_ENDPOINT = 'https://mainnet-v4.tonhubapi.com'
-  static MAINNET_INDEXER_ENDPOINT = 'https://indexer.torch.finance'
-  static MAINNET_ORACLE_ENDPOINT = 'https://oracle.torch.finance'
-}
--- packages/torch-sdk/src/utils/unit.ts
+++
@@ -1,9 +0,0 @@
-import Decimal from 'decimal.js'
-export const toUnit = (
-  value: Decimal.Value | bigint,
-  decimals: number | bigint = 9,
-): bigint => {
-  const v = value.toString()
-  const d = decimals.toString()
-  return BigInt(new Decimal(v).mul(Decimal.pow(10, d)).toFixed(0))
-}
